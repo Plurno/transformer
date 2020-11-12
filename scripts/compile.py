7c63bec0b544c6fc9a37f037e1eb754963c2cd27
Ryan Hockstad <ryanhockstad@gmail.com>
2020-11-11 19:57:34 -0500

    testing neuron compiler on tokenizers

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,31 +1,34 @@
-# from haystack import Finder
-# from haystack.reader.farm import FARMReader
-# # from haystack.reader.transformers import TransformersReader
-# from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
-# from haystack.retriever.sparse import ElasticsearchRetriever
-# from haystack.utils import print_answers
-
-
-# document_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="ahrq")
-
-# reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-# # reader = FARMReader(model_name_or_path="deepset/minilm-uncased-squad2", use_gpu=False)
-
-# retriever = ElasticsearchRetriever(document_store=document_store)
-# finder = Finder(reader, retriever)
-
-# prediction = finder.get_answers(question="Does ahrq offer minority supplements for grants?", top_k_retriever=10, top_k_reader=5)
-# print_answers(prediction, details="medium")
-
 import torch
-import numpy as np
-import os
-import torch_neuron
+# import torch_neuron
+# import numpy as np
+# import os
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
-from haystack.reader.farm import FARMReader
-reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-model = reader.inferencer.model
+# model_name = "deepset/roberta-base-squad2" # Model for QA
+# model_name = "sshleifer/tiny-distilbert-base-uncased-finetuned-sst-2-english" # Tiny, untrained model for sentiment analysis
+model_name = "distilbert-base-uncased-finetuned-sst-2-english" # Sentiment Model
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model.eval()
-test_dict = {}
-model_neuron = torch.neuron.trace(model, example_inputs=test_dict)
+inputs = tokenizer("We are very happy to show you the 🤗 Transformers library.")
+
+pt_batch = tokenizer(
+     ["i am happy", "i am sad"],
+     padding=True,
+     truncation=True,
+     return_tensors="pt"
+    )
+
+# pt_outputs = model(**pt_batch)
+# pt_predictions = F.softmax(pt_outputs[0], dim=1)
+# print(pt_predictions)
+
+model_neuron = torch.neuron.trace(model, example_inputs=[pt_batch])
+## Export to saved model
+model_neuron.save("model.pt")
+
+tokenizer_neuron = torch.neuron.trace(tokenizer)
+tokenizer_neuron.save('tokenizer.pt')
+
+# Now try with pipeline
--- scripts/huggingface.py
+++ scripts/huggingface.py
@@ -1,1 +1,3 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model = 'sshleifer/tiny-distilbert-base-uncased-finetuned-sst-2-english'
